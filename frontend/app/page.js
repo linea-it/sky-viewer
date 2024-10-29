aaf8a478fd8d7f409a24094cf82573f2ac9db07b
jandsonrj <jandson1512@gmail.com>
2024-10-29 21:46:43 +0000

    feat(fixes-on-pages): fixes in calculating pages with header and footer

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -10,7 +10,10 @@ import dynamic from 'next/dynamic'
 
 export default function Home() {
   return (
-    <Box id={'home-box'} sx={{ flexGrow: 1 }}>
+    <Box id={'home-box'} sx={{
+      flexGrow: 1,
+      minHeight: 'calc(100vh - 64px - 32px)'
+    }}>
       {/* SE tiver mais componentes tipo menu de escolha dos releases vai ficar aqui.*/}
       {typeof window === "undefined" ? (<div>loading...</div>) : (<Aladin />)}
     </Box>
